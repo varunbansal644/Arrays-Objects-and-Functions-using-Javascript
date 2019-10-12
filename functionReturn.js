c9c9edf14dd791ed38b19cd54a2bcab682700144
varunbansal644 <varunbansal644@gmail.com>
2019-10-12 19:51:50 +0530

    Function with Return

---
+++ functionReturn.js
@@ -0,0 +1,9 @@
+// Declare the function here, name the function returnStringFunction
+function returnStringFunction(){
+ return("Hi I am being returned by a function");
+}
+
+// Store the value in a variable named returnedString
+var returnedString = returnStringFunction();
+// Print the variable
+console.log(returnedString);
