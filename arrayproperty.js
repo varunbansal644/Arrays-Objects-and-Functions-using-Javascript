139bf37d9631af9ccdf4b6c5f33ba5796866e727
varunbansal644 <varunbansal644@gmail.com>
2019-10-12 19:43:58 +0530

    Properties of Array

---
+++ arrayproperty.js
@@ -0,0 +1,14 @@
+var carNames = ['Honda','Ferrari','Porsche','Fiat'];
+// You have to print the array carNames after you perform each operation.
+// Remove Fiat from the end of the array.
+carNames.pop();
+console.log(carNames);
+// Add Mazda to the end of the array.
+carNames.push('Mazda');
+console.log(carNames);
+// Remove Honda from the front of the array.
+carNames.shift();
+console.log(carNames);
+// Add Ford to the front of the array.
+carNames.unshift('Ford');
+console.log(carNames);
