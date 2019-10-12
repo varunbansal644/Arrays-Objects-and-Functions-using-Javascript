6cfb5523194ff790946dfe5fcc45b1914b1c2303
varunbansal644 <varunbansal644@gmail.com>
2019-10-12 19:50:28 +0530

    Function Parameters

---
+++ functionWithparameters.js
@@ -0,0 +1,9 @@
+// Declare the function here, name it passParameterFunction
+
+function passParameterFuntion(param){
+ console.log("Hi I am a function that has been passed a parameter -",param);
+}
+
+// Invoke the function and don’t forget to pass a parameter
+
+passParameterFuntion('Parameter');
