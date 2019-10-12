f962a9a10b073383421e8c2f163bd8e406ccee90
varunbansal644 <varunbansal644@gmail.com>
2019-10-12 19:47:06 +0530

    Object Modification

---
+++ objectmodify.js
@@ -0,0 +1,10 @@
+// Updating the value of two keys testKey1 and 'test-key2' to testValue1New and testValue2New respectively
+var testObject = {
+  testKey1 : 'testValue1',
+  'test-Key2' : 'testValue2'
+};
+var testObject = {
+    testKey1 : 'testValue1New',
+    'test-Key2' : 'testValue2New'
+}
+console.log(testObject);
