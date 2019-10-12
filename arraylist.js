c95871074bfa571a25487eb67b5d97b138df8c2c
varunbansal644 <varunbansal644@gmail.com>
2019-10-12 19:42:50 +0530

    Array Shopping List

---
+++ arraylist.js
@@ -0,0 +1,8 @@
+// Create an array in which you need to add the following items and their required numbers. Name the array shoppingList
+// Milk - 2
+// Bread - 3
+// Potato - 12
+console.log("Shopping List -");
+console.log("Milk-2");
+console.log("Bread-3");
+console.log("Potato-12");
